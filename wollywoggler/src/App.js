0414b01519dd9e0f20a5a91862727f1a6ceac560
kelsey <enktal@gmail.com>
2023-02-20 15:04:23 -0800

    fix build again

--- wollywoggler/src/App.js
+++ wollywoggler/src/App.js
@@ -24,19 +24,6 @@ function App() {
   );
 }
 
-//Simple page with buttons using components
-function WogglerApp() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Wolly Woggler</h1>
-        <p>Wolly Woggler is a simple app to help you decide what to wear.</p>
-        <p>Click the button below to get started.</p>
-        <Button />
-      </header>
-    </div>
-  );
-}
 
 
 export default Hero;
